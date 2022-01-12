37cc1ae2d0ca124998a286744e117507caf25101
AnesVijay <2011andreyka@gmail.com>
2022-01-12 19:31:23 +0500

    PausePanel (mvp)

--- WildModel/Assets/Scripts/PauseManager.cs
+++ WildModel/Assets/Scripts/PauseManager.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PauseManager : MonoBehaviour
 {
-
     public void OnApplicationPause(bool pause)
     {
-            if (pause)
-            {
-                Time.timeScale = 0f;
-            }
-            else
-            {
-                Time.timeScale = 1f;
-            }
+        if (pause)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
     }
 }
